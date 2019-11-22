8b2afabde7dfbefb2ae5d2b92005581a703779ed
darexnigga <dgleon1@espe.edu.ec>
2019-11-22 00:05:55 -0500

    Historia de Usuario #14 - Correccion Controlador

--- src/main/java/com/ficha/medica/app/web/controllers/MetodosPlanificacionMasculinoController.java
+++ src/main/java/com/ficha/medica/app/web/controllers/MetodosPlanificacionMasculinoController.java
@@ -15,71 +15,69 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.ficha.medica.app.web.models.entities.MetodosPlanificacionMasculino;
 import com.ficha.medica.app.web.models.service.IMetodosPlanificacionMasculinoService;
 
+
+@Controller
+@RequestMapping(value="/metodosplanificacionmasculino")
 public class MetodosPlanificacionMasculinoController {
 
-	@Controller
-	@RequestMapping(value="/metodosplanificacionmasculino")
-	public class metodosplanificacionmasculinoController {
-
-		@Autowired
-		private IMetodosPlanificacionMasculinoService service;
+	@Autowired
+	private IMetodosPlanificacionMasculinoService service;
 		
-		@GetMapping(value="/create")
-		public String create(Model model) {
-			MetodosPlanificacionMasculino metodosplanificacionmasculino = new MetodosPlanificacionMasculino();
-			model.addAttribute("title", "Registro de un Método de Planificación Familiar Masculino");
-			model.addAttribute("metodosplanificacionmasculino", metodosplanificacionmasculino);
-			return "metodosplanificacionmasculino/form";		
-		}
-		
-		@GetMapping(value="/retrieve/{id}")
-		public String retrieve(@PathVariable(value="id") Integer id, Model model) {
-			MetodosPlanificacionMasculino metodosplanificacionmasculino = service.findById(id);
-			model.addAttribute("metodosplanificacionmasculino", metodosplanificacionmasculino);
-			return "metodosplanificacionmasculino/card";		
-		} 
-		
-		@GetMapping(value="/update/{id}")
-		public String update(@PathVariable(value="id") Integer id, Model model) {
-			MetodosPlanificacionMasculino metodosplanificacionmasculino = service.findById(id);
-			model.addAttribute("title", "Actualizando el registro de " + metodosplanificacionmasculino.getNombre());
-			model.addAttribute("metodosplanificacionmasculino", metodosplanificacionmasculino);
-			return "metodosplanificacionmasculino/form";		
-		} 
-		
-		@GetMapping(value="/delete/{id}")
-		public String delete(@PathVariable(value="id") Integer id, Model model, 
-				RedirectAttributes flash) {
-			try {
-				service.delete(id);
-				flash.addFlashAttribute("success", "El registro fue eliminado con éxito.");
-			}	
-			catch(Exception ex) {
-				flash.addFlashAttribute("error", "El registro no pudo ser eliminado.");
-			}
-			return "redirect:/metodosplanificacionmasculino/list";		
-		} 
-		
-		@PostMapping(value="/save")
-		public String save(MetodosPlanificacionMasculino metodosplanificacionmasculino, Model model,
-				RedirectAttributes flash) {
-			try {
-				service.save(metodosplanificacionmasculino);
-				flash.addFlashAttribute("success", "El registro fue guardado con éxito.");
-			}
-			catch(Exception ex) {
-				flash.addFlashAttribute("error", "El registro no pudo ser guardado.");
-			}
-			return "redirect:/metodosplanificacionmasculino/list";		
-		} 
-		
-		@GetMapping(value="/list")
-		public String list(Model model) {
-			List<MetodosPlanificacionMasculino> lista = service.findAll();
-			model.addAttribute("title", "Listado de Métodos de Planificación Familiar Masculino");
-			model.addAttribute("lista", lista);
-			return "metodosplanificacionmasculino/list";		
-		}
-	
+	@GetMapping(value="/create")
+	public String create(Model model) {
+		MetodosPlanificacionMasculino metodosplanificacionmasculino = new MetodosPlanificacionMasculino();
+		model.addAttribute("title", "Registro de un Método de Planificación Familiar Masculino");
+		model.addAttribute("metodosplanificacionmasculino", metodosplanificacionmasculino);
+		return "metodosplanificacionmasculino/form";		
 	}
+		
+	@GetMapping(value="/retrieve/{id}")
+	public String retrieve(@PathVariable(value="id") Integer id, Model model) {
+		MetodosPlanificacionMasculino metodosplanificacionmasculino = service.findById(id);
+		model.addAttribute("metodosplanificacionmasculino", metodosplanificacionmasculino);
+		return "metodosplanificacionmasculino/card";		
+	} 
+		
+	@GetMapping(value="/update/{id}")
+	public String update(@PathVariable(value="id") Integer id, Model model) {
+		MetodosPlanificacionMasculino metodosplanificacionmasculino = service.findById(id);
+		model.addAttribute("title", "Actualizando el registro de " + metodosplanificacionmasculino.getNombre());
+		model.addAttribute("metodosplanificacionmasculino", metodosplanificacionmasculino);
+		return "metodosplanificacionmasculino/form";		
+	} 
+		
+	@GetMapping(value="/delete/{id}")
+	public String delete(@PathVariable(value="id") Integer id, Model model, 
+			RedirectAttributes flash) {
+		try {
+			service.delete(id);
+			flash.addFlashAttribute("success", "El registro fue eliminado con éxito.");
+		}	
+		catch(Exception ex) {
+			flash.addFlashAttribute("error", "El registro no pudo ser eliminado.");
+		}
+		return "redirect:/metodosplanificacionmasculino/list";		
+	} 
+		
+	@PostMapping(value="/save")
+	public String save(MetodosPlanificacionMasculino metodosplanificacionmasculino, Model model,
+			RedirectAttributes flash) {
+		try {
+			service.save(metodosplanificacionmasculino);
+			flash.addFlashAttribute("success", "El registro fue guardado con éxito.");
+		}
+		catch(Exception ex) {
+			flash.addFlashAttribute("error", "El registro no pudo ser guardado.");
+		}
+		return "redirect:/metodosplanificacionmasculino/list";		
+	} 
+		
+	@GetMapping(value="/list")
+	public String list(Model model) {
+		List<MetodosPlanificacionMasculino> lista = service.findAll();
+		model.addAttribute("title", "Listado de Métodos de Planificación Familiar Masculino");
+		model.addAttribute("lista", lista);
+		return "metodosplanificacionmasculino/list";		
+	}
+	
 }
